328a88f6a18ca77fc035e3e5f5f95b7eadd1e6d1
ce1okr <josh.hyunjin@gmail.com>
2026-09-24 22:55:28 -0400

    Refactor configuration handling and add new commands

--- src/main/java/com/empoweredsmp/EmpoweredSMP.java
+++ src/main/java/com/empoweredsmp/EmpoweredSMP.java
@@ -3,6 +3,8 @@ package com.empoweredsmp;
 import com.empoweredsmp.commands.ClassCommand;
 import com.empoweredsmp.commands.ExtraInventoryCommand;
 import com.empoweredsmp.commands.EmpoweredAdminCommand;
+import com.empoweredsmp.commands.BowChoiceCommand;
+import com.empoweredsmp.commands.ToolChoiceCommand;
 import com.empoweredsmp.commands.UnlockCommand;
 import com.empoweredsmp.data.DataManager;
 import com.empoweredsmp.data.GlobalState;
@@ -13,6 +15,7 @@ import com.empoweredsmp.listeners.EnforcementListener;
 import com.empoweredsmp.listeners.ExtraInventoryListener;
 import com.empoweredsmp.listeners.FragmentInteractListener;
 import com.empoweredsmp.listeners.LungeListener;
+import com.empoweredsmp.listeners.RangerCrossbowListener;
 import com.empoweredsmp.listeners.SurgeListener;
 import com.empoweredsmp.listeners.VillagerListener;
 import com.empoweredsmp.listeners.WorldAccessListener;
@@ -42,19 +45,19 @@ public class EmpoweredSMP extends JavaPlugin {
     private CooldownManager cooldownManager;
     private ExtraInventoryManager extraInventoryManager;
     private EffectManager effectManager;
-    private Config config;
+    private Config pConfig;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         Keys.init(this);
 
-        config = new Config(getConfig());
+        pConfig = new Config(getConfig());
         dataManager = new DataManager(this);
         globalState = new GlobalState(this);
         abilityManager = new AbilityManager(dataManager);
         cooldownManager = new CooldownManager();
-        extraInventoryManager = new ExtraInventoryManager(dataManager, abilityManager, config);
+        extraInventoryManager = new ExtraInventoryManager(dataManager, abilityManager, pConfig);
         StarterKitManager starterKitManager = new StarterKitManager();
 
         // Recipe
@@ -63,27 +66,30 @@ public class EmpoweredSMP extends JavaPlugin {
         // Listeners
         var pm = Bukkit.getPluginManager();
         pm.registerEvents(new DeathListener(abilityManager, dataManager, extraInventoryManager, LEVEL_FRAGMENT_MATERIAL), this);
-        pm.registerEvents(new FragmentInteractListener(abilityManager, dataManager), this);
-        pm.registerEvents(new EnforcementListener(abilityManager, cooldownManager, config), this);
-        pm.registerEvents(new SurgeListener(this, abilityManager, cooldownManager, config), this);
-        pm.registerEvents(new CombatListener(abilityManager, config), this);
+        pm.registerEvents(new FragmentInteractListener(abilityManager, dataManager, starterKitManager), this);
+        pm.registerEvents(new EnforcementListener(abilityManager, cooldownManager, pConfig), this);
+        pm.registerEvents(new SurgeListener(this, abilityManager, cooldownManager, pConfig), this);
+        pm.registerEvents(new CombatListener(abilityManager, pConfig), this);
         pm.registerEvents(new VillagerListener(abilityManager, globalState), this);
         pm.registerEvents(new WorldAccessListener(abilityManager, globalState), this);
-        pm.registerEvents(new EnderPearlListener(cooldownManager, config), this);
+        pm.registerEvents(new EnderPearlListener(cooldownManager, pConfig), this);
         pm.registerEvents(new LungeListener(cooldownManager), this);
         pm.registerEvents(new ExtraInventoryListener(extraInventoryManager), this);
+        pm.registerEvents(new RangerCrossbowListener(), this);
 
         // Commands
         getCommand("class").setExecutor(new ClassCommand(abilityManager, starterKitManager));
         getCommand("ei").setExecutor(new ExtraInventoryCommand(extraInventoryManager));
-        getCommand("empowered").setExecutor(new EmpoweredAdminCommand(abilityManager, dataManager));
+        getCommand("empowered").setExecutor(new EmpoweredAdminCommand(abilityManager, dataManager, starterKitManager));
+        getCommand("bowchoice").setExecutor(new BowChoiceCommand(abilityManager));
+        getCommand("toolchoice").setExecutor(new ToolChoiceCommand(abilityManager));
         UnlockCommand unlock = new UnlockCommand(globalState);
         getCommand("unlocknether").setExecutor(unlock);
         getCommand("unlockend").setExecutor(unlock);
         getCommand("unlockvillagers").setExecutor(unlock);
 
         // Passive effect ticking
-        effectManager = new EffectManager(this, abilityManager, config);
+        effectManager = new EffectManager(this, abilityManager, pConfig);
         effectManager.start();
 
         getLogger().info("EmpoweredSMP enabled.");
